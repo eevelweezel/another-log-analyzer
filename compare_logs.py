7b20c218414776713db66d5aa5e1198651f77177
eevel <eevel.weezel@gmail.com>
2013-08-06 00:27:27 -0500

    refactored

--- compare_logs.py
+++ compare_logs.py
@@ -2,10 +2,9 @@
 # Gen a list of these for each input
 # Run a diff for stuff that executes in one log and not the other
 # ... maybe allow comparison a block at a time?
-
+#
 # ActiveLink: CDB:EM-SetJournalDetails - Fri Jul 19 2013 10:55:00 AM
 # note ": " and " -" denote start/end of name & line begins w/ "Active Link"
-
 
 # set inputs
 print('Logfile 1 and path:')
@@ -18,25 +17,32 @@ z = input()
 log_output = open(z,'w')
 
 # loop through logs & parse into lists
-def parse_this(a):
-	lines_in[]
-	log = open(a, 'r')
-	for line in log:
-		lines_x.append(log.readline())
-	a.close()
-	return lines_in
+def parse_stuff(a):
+    log = open(a, 'r')
+    lines_in = log.readlines()
+    actl = []
+    fltr = []
+#    sql = []
+#    api = []
+    for line in lines_in:
+        if line.startswith('Active Link',0,len(line)):
+            actl.append(line[line.find(':',0,len(line)):line.find('-',0,len(line))])
+        elif line.startswith('<FLTR>',0,len(line)):
+            fltr.append(line)
+#        elif line.startswith('<SQL >',0,len(line)):
+#            sql.append(str(line[0:line.index(len(line))]))
+#        elif line.startswith('<API >',0,len(line)):
+#            api.append(str(line[0:line.index(len(line))]))
+    return lines_in, actl, fltr #, sql, api	
 
-# gen a list for active link execution	
-def active_links(lines_in):
-	actl[]
-	for line in lines_in:
-		if line.startswith('Active Link',0,len(line))
-			actl.append(line.tell()+' '+(line[line.index(':'):line.index('-')]))
-	return actl		
-			
+parse_stuff(x)
+#a_links(lines_in)
+#print(str(actl))
+
+"""			
 # gen a list for filter execution	
 def filters(lines_in):
-	fltr[]
+	fltr = []
 	for line in lines_in:
 		if line.startswith('Filter',0,len(line))
 			actl.append(line.tell()+' '+(line[line.index(':'):line.index('-')]))
@@ -44,7 +50,7 @@ def filters(lines_in):
 			
 # gen a list for SQL execution	
 def sql(lines_in):
-	sql_st[]
+	sql_st = []
 	for line in lines_in:
 		if line.startswith('<SQL>',0,len(line))
 			actl.append(line.tell()+' '+(line[line.index(':'):line.index('-')]))
@@ -74,5 +80,5 @@ process_log(lines_y)
 
 # cleanup
 log_output.close()
-quit()	
+"""
 
